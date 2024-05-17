2cbcf633e0a8c657f8591f12964eb81c7b359c54
Your Name <aqatra6@gmail.com>
2024-05-17 08:06:00 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,13 @@
+function superbowlWin(record) {
+    const winningRecord = record.find(game => game.result === 'W');
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
+  const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    
+  ];
+  
+  console.log(superbowlWin(record));
